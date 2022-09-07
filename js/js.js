cf76b44255815b73eca152f94a63ff5ace27f17b
MaruSosa2004 <marusosa2004@gmail.com>
2022-09-06 23:51:18 -0300

    final 2

---
+++ js/js.js
@@ -0,0 +1,42 @@
+const divProductos = document.getElementById("divProductos")
+const botonProductos = document.getElementById("botonProductos")
+const inputProducto = document.getElementById("inputProducto")
+
+const traerProductos = async () => {
+    const response = await fetch('./json/photocards.json')
+    const productos = await response.json()
+    return productos
+}
+
+function mostrarProductos(arrayProductos) {
+    arrayProductos.forEach((producto, indice) => {
+        divProductos.innerHTML += `
+            <div class="card" id="producto${indice}" style="width: 18rem;margin:3px;">
+            <img src="./img/${producto.img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">Modelo: ${producto.modelo}</p>
+                    <p class="card-text">Año: ${producto.año}</p>
+                    <p class="card-text">Stock Actual: ${producto.stock}</p>
+                    <button class="btn btn-outline-primary">Agregar a la colección</button>
+                </div>
+            </div>
+            `
+    });
+}
+
+inputProducto.addEventListener('input', () => {
+    let res = inputProducto.value
+   
+    traerProductos().then(productos => {
+        const productosFiltrados = productos.filter(producto => producto.nombre.toLowerCase().includes(res.toLowerCase()))
+        divProductos.innerHTML = ""
+        mostrarProductos(productosFiltrados)
+    })
+})
+
+botonProductos.addEventListener('click', () => {
+    traerProductos().then(productos => {
+        mostrarProductos(productos)
+    })
+})
